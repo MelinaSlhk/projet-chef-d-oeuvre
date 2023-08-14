9333ef06c05f33b30915f4b737577acfe9fc0e41
MelinaSlhk <melinaloony@gmail.com>
2023-08-14 14:29:30 +0200

    3 - routing principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { PageMyPaintingComponent } from './pages/page-my-painting/page-my-painting.component';
+import { PageAdminComponent } from './pages/page-admin/page-admin.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, //si un user arrive sur localhost:4200 on le redirige vers localhost:4200/home
+  { path: 'home', component: PageHomeComponent },
+  { path: 'my-painting', component: PageMyPaintingComponent },
+  { path: 'admin', component: PageAdminComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
